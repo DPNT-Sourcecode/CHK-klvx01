a70163c722e791d72a603701885674060fa7d332
sbx_user1051 <sbx_user1051@169.254.176.121>
2023-05-24 16:18:20 +0000

    Wed May 24 16:18:20 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,13 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
+
+# In this function we are getting the user to implement a friend's name and saying hello to them and the world
 def hello(friend_name):
-    raise NotImplementedError()
+    # Check the input is a string
+    if not isinstance(friend_name,str):
+        raise TypeError(f"Expected string value, but received friend_name={friend_name} (type: {type(friend_name).__name__})")
+    # Say hello to the inputted string
+    else:
+        return f"Hello {friend_name}! Welcome and say hello to the world!"
+
