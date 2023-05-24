0bfe3afb1902849a0185283113b5284ab2b6f086
sbx_user1051 <sbx_user1051@169.254.176.121>
2023-05-24 16:21:45 +0000

    Wed May 24 16:21:45 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -10,7 +10,8 @@ def hello(friend_name):
         raise TypeError(f"Expected string value, but received friend_name={friend_name} (type: {type(friend_name).__name__})")
     # Say hello to the inputted string
     else:
-        return f"Hello, World!"
+        return f"Hello, {friend_name}!"
+
 
 
 
